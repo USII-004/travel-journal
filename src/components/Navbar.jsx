c7ee1e88c7a6698d03310cfdb2cb3f2ea9128333
usii-004 <usmansmooth004@gmail.com>
2023-10-17 21:42:16 +0100

    halfly done webpage

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className='h-16 md:h-20 bg-[#F55A5A] mx-auto justify-center py-5 md:py-6 flex mb-4'>
+        <p className='text-white md:text-2xl'>My travel Journal.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/components/Places.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {FaEarthAmericas, FaLocationDot} from "react-icons/fa6";
+
+
+const Places = (props) => {
+  return (
+    <div className='py-4 px-8 md:px-24 md:py-8'>
+      <div className='md:flex'>
+        <img src={props.imageUrl} alt='/' className='h-[250px] md:h-[400px] md:w-[350px] rounded-md' />
+        <div>
+          <div className='flex mt-4'>
+            <FaLocationDot  className='text-[#F55A5A]'/>
+            <p className='px-2 text-[#918E9B] text-sm md:text-lg'>{props.location}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Places
