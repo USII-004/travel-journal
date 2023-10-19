907efd78854fcc1ec5d7ae2e117fe190425fcf73
usii-004 <usmansmooth004@gmail.com>
2023-10-19 23:51:15 +0100

    small update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div>
       <div className='h-16 md:h-20 bg-[#F55A5A] mx-auto justify-center py-5 md:py-6 flex mb-4'>
-        <FaEarthAmericas />
+        <FaEarthAmericas size={30} className='text-white mx-4' />
         <p className='text-white md:text-2xl'>My travel Journal.</p>
       </div>
     </div>
